48f041b6ac0098e53b31878b21ca887659a2bb30
Laboratorios <Laboratorios@host.docker.internal>
2026-02-10 18:31:29 -0600

    Proyecto 1

---
+++ src/main/java/mx/edu/itson/proyecto1/PersistenciaPacientes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.edu.itson.proyecto1;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class PersistenciaPacientes {
+
+}
